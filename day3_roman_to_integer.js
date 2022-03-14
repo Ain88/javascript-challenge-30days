97936f4aed7047e4991fb82b8f2b7cc7c3b05732
ain88 <ahyungss@hotmail.com>
2022-03-13 19:43:43 -0700

    Update day3_roman_to_integer.js

--- day3_roman_to_integer.js
+++ day3_roman_to_integer.js
@@ -3,26 +3,36 @@ https://leetcode.com/problems/roman-to-integer
  * @param {string} s
  * @return {number}
  */
+ let decimals = {
+    'I'  : 1,
+    'IV' : 4,
+    'IX' : 9,
+    'V'  : 5,
+    'X'  : 10,
+    'XL' : 40,
+    'XC' : 90,
+    'L'  : 50,
+    'C'  : 100,
+    'CD' : 400,
+    'CM' : 900,
+    'D'  : 500,
+    'M'  : 1000,
+ }
 var romanToInt = function(s) {
-    var key = {
-        "I": 1,
-        "V": 5,
-        "X": 10,
-        "L": 50,
-        "C": 100,
-        "D": 500,
-        "M": 1000
-    }
-    var total = 0
-    s = s.split("")
-    for (var i = 0; i < s.length; i++) {
-        if(key[s[i]]<key[s[i+1]]) {
-            total += key[s[i+1]]-key[s[i]]
-            i++
-        } else{
-            total += key[s[i]]
+    let dec = 0;
+    for(let i = 0 ; i < s.length ; i++){
+		// if the next char is sequence of the special combination, then use  those to find in the dictionary the value
+        if(
+            s.charAt(i) == 'I' && (s.charAt(i+1) == 'X' || s.charAt(i+1) == 'V')
+            || 
+            s.charAt(i) == 'X' && (s.charAt(i+1) == 'L' || s.charAt(i+1) == 'C')
+            || 
+            s.charAt(i) == 'C' && (s.charAt(i+1) == 'D' || s.charAt(i+1) == 'M')
+        ){
+            dec += decimals[s.charAt(i) + s.charAt(i+1)]
+            i++;
+        }else{
+            dec += decimals[s.charAt(i)]
         }
-    }   
-    return total
-};
-
+    }
+    retu
