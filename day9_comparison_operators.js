f955ed61655e1f6a1b91da056e6ee453fa2fd6e2
ain88 <ahyungss@hotmail.com>
2022-03-19 17:58:18 -0700

    Create day9_comparison_operators.js

---
+++ day9_comparison_operators.js
@@ -0,0 +1,7 @@
+//https://www.jschallenger.com/javascript-basics/comparison-strict-equality
+// Write a function that takes two values, say a and b, as arguments
+// Return true if the two values are equal and of the same type
+
+function myFunction(a, b) {
+  return a === b;
+}
