7020130535d9f4f2700c900db75c3d6efb784200
ain88 <ahyungss@hotmail.com>
2022-03-19 17:58:53 -0700

    Update day8_sum_two_numbers.js

--- day8_sum_two_numbers.js
+++ day8_sum_two_numbers.js
@@ -1,14 +1,8 @@
-https://www.jschallenger.com/javascript-basics/sum-two-numbers-javascript
-
+//https://www.jschallenger.com/javascript-basics/sum-two-numbers-javascript
 // Write a function that takes two numbers (a and b) as argument
 // Sum a and b
 // Return the result
-function
-myFunction
-(
-a, b
-)
-{
 
-return a + b
+function myFunction(a, b) {
+   return a + b;
 }
